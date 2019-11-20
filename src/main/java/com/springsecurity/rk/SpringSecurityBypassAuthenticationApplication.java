35242cb68cb106d62870a783f64b82829b3d19e6
Rahul Kumar <rahul.rk0627@gmail.com>
2019-11-20 23:19:42 +0530

    initial commit

---
+++ src/main/java/com/springsecurity/rk/SpringSecurityBypassAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.rk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBypassAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBypassAuthenticationApplication.class, args);
+	}
+
+}
